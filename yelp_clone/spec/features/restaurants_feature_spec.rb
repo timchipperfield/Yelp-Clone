a280321566dd2e1a58d98d5b2154ce88608f2bac
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-13 15:08:45 +0100

    second test passing

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -1,9 +1,23 @@
 feature "restaurants" do
+
   context "no restaurants have been added" do
+
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_content 'Add a restaurant'
+    end
+
+  end
+
+  context "restaurant have been created" do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+    scenario "display restaurant" do
+      visit '/restaurants'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('No restaurants yet')
     end
   end
 end
